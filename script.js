b9e6697b6e32cd1e7070f8fa8fc4cecc8bd9b797
Peter Andrew <peter.a.andrew@gmail.com>
2024-07-07 17:11:28 +0100

    added a monster loosing health function

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const prompt = require('prompt-sync')();
 let randomD12Num;
 let heroAttackStrength;
 let monsterAttackStrength;
+let currentMonsterStamina
+let currentHeroStamina = 12;
 
-function Player (type,skill,stamina,luck,damage,speed,toughness,armour,items) {
+function Player(type, skill, stamina, luck, damage, speed, toughness, armour, items) {
     this.type = type;
     this.skill = skill;
     this.stamina = stamina;
@@ -15,7 +17,7 @@ function Player (type,skill,stamina,luck,damage,speed,toughness,armour,items) {
     this.items = items;
 }
 
-function Monster (type,skill,stamina,luck,damage,speed,toughness,armour,items) {
+function Monster(type, skill, stamina, luck, damage, speed, toughness, armour, items) {
     this.type = type;
     this.skill = skill;
     this.stamina = stamina;
@@ -27,63 +29,82 @@ function Monster (type,skill,stamina,luck,damage,speed,toughness,armour,items) {
     this.items = items;
 }
 
-let hero = new Player ("elf",/*skill*/12,/*stamina*/12,/*luck*/12,/*damage*/4,/*speed*/10,/*toughness*/6,/*armour*/2,["sword","shield","leather armour"]);
-let lizardman = new Monster ("Lizardman",/*skill*/12,/*stamina*/14,/*luck*/10,/*damage*/4,/*speed*/10,/*toughness*/7,/*armour*/4,["halberd", "chainmail"]);
-let beastman = new Monster ("beastman",8,16,8,4,6,10,2["axe","leather armour"]);
-let ogre = new Monster ("Ogre",7,16,7,5,6,12,4);
+let hero = new Player("elf",/*skill*/12,/*stamina*/12,/*luck*/12,/*damage*/4,/*speed*/10,/*toughness*/6,/*armour*/2, ["sword", "shield", "leather armour"]);
+let lizardman = new Monster("Lizardman",/*skill*/12,/*stamina*/14,/*luck*/10,/*damage*/4,/*speed*/10,/*toughness*/7,/*armour*/4, ["halberd", "chainmail"]);
+let beastman = new Monster("beastman", 8, 16, 8, 4, 6, 10, 2["axe", "leather armour"]);
+let ogre = new Monster("Ogre", 7, 16, 7, 5, 6, 12, 4);
 
 //array of all possible monsters
-let possibleMonsters = [lizardman,beastman,ogre];
+let possibleMonsters = [lizardman, beastman, ogre];
 // chooses a random monster, this sets the index value
 let randomMonsterIndex = Math.floor(Math.random() * possibleMonsters.length);
-// console.log("random Monster index = " + randomMonsterIndex);
+
 //gets the name field for the random monster
 let randomMonsterType = possibleMonsters[randomMonsterIndex].type;
 console.log("random Monster is a = " + randomMonsterType);
 
 // updates the global random D12 variable each time it is called
-function randomD12 () {
-    randomD12Num = parseInt(Math.floor(Math.random() * 11) + 2);   
+function randomD12() {
+    randomD12Num = parseInt(Math.floor(Math.random() * 11) + 2);
     // console.log("random D12 = " + randomD12Num);
 }
 
 function getHeroAttackStrength() {
     randomD12();
-    heroAttackStrength = hero.skill+randomD12Num;
+    heroAttackStrength = hero.skill + randomD12Num;
     console.log("Hero attack strength = " + heroAttackStrength);
 }
 
 function getMonsterAttackStrength() {
     randomD12();
-    monsterAttackStrength = possibleMonsters[randomMonsterIndex].skill+randomD12Num;
+    monsterAttackStrength = possibleMonsters[randomMonsterIndex].skill + randomD12Num;
     console.log("Monster attack strength = " + monsterAttackStrength);
 }
 
-function fight () {
-    
-    getHeroAttackStrength();
-    getMonsterAttackStrength();
-    console.log("monster is a " + randomMonsterType);
-    
-    if (heroAttackStrength > monsterAttackStrength) {
-        console.log("hero wounds! Monster goes arrrrrgghhh")
-        let currentMonsterStamina = (possibleMonsters[randomMonsterIndex].stamina)-2;
-        // need to update the specified monster stamina
-        console.log("monster stamina = " + currentMonsterStamina);
-        fightAgainYorN();
 
+function fight() {
 
+    if (currentMonsterStamina == 0 || currentMonsterStamina < 0) {
+        console.log("The monster lies dead at your feet");
+    } else if (currentHeroStamina == 0 || currentHeroStamina < 0) {
+        console.log("You Died");
     } else {
-        console.log("monster wounds the hero, eeeeek!")
-        fightAgainYorN();
 
+        getHeroAttackStrength();
+        getMonsterAttackStrength();
+        console.log("monster is a " + randomMonsterType);
+
+        if (heroAttackStrength > monsterAttackStrength) {
+            console.log("hero wounds! Monster goes arrrrrgghhh")
+            currentMonsterStamina = (possibleMonsters[randomMonsterIndex].stamina);
+
+            //updates the specified monster stamina as the fight goes on
+            possibleMonsters[randomMonsterIndex].stamina = currentMonsterStamina - 2;
+            console.log("monster stamina = " + currentMonsterStamina);
+            if (currentMonsterStamina == 0 || currentMonsterStamina < 0) {
+                console.log("The monster lies dead at your feet.");
+                return
+            }
+            // asks if you want to fight again
+            fightAgainYorN();
+
+        } else if (heroAttackStrength == monsterAttackStrength) {
+            console.log("attack strength are equal, this round is a draw");
+            fightAgainYorN();
+        }
+        else {
+            console.log("monster wounds the hero, eeeeek!")
+            // reduce hero health 
+            fightAgainYorN();
+        }
     }
 }
+
 fight();
 
 //ask if you want to fight or flee
-function fightAgainYorN () {    
-    const name = prompt('Fight (Y) or flee (N)?');        
+function fightAgainYorN() {
+    const name = prompt('Fight (Y) or flee (N)?');
     let answer = `${name}`;
     console.log(answer);
 
@@ -92,10 +113,10 @@ function fightAgainYorN () {
         fight();
     } else if (answer === "N" || answer === "n") {
         console.log("Run for your life!");
-    }  else {
+    } else {
         console.log("that's not a Y/N answer, please choose again");
         fightAgainYorN();
-    } 
+    }
 }
 
 
