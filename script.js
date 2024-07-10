fec5b65c4e25f5a7c085d673579d0dfd1cdbbc1f
Peter Andrew <peter.a.andrew@gmail.com>
2024-07-10 13:49:18 +0100

    added an items menu

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let monsterAttackStrength;
 let currentMonsterStamina
 let currentHeroStamina = 12;
 let battleRound = 0;
+let itemResponse;
 
 function Player(type, skill, stamina, luck, damage, speed, toughness, armour, items) {
     this.type = type;
@@ -37,56 +38,56 @@ function Monster(type, skill, stamina, luck, damage, speed, toughness, armour, i
 
 //build an object for weapons
 let weapons = {
-    sword:{
-        skill:4,
-        speed:6,
-        damage:2,
-        rend:2
+    sword: {
+        skill: 4,
+        speed: 6,
+        damage: 2,
+        rend: 2
     },
-    axe:{
-        skill:3,
-        speed:6,
-        damage:2,
-        rend:2,
+    axe: {
+        skill: 3,
+        speed: 6,
+        damage: 2,
+        rend: 2,
     },
-    knife:{
-        skill:2,
-        speed:6,
-        damage:1,
-        rend:1,
+    knife: {
+        skill: 2,
+        speed: 6,
+        damage: 1,
+        rend: 1,
     },
     mace: {
         skill: 2,
         speed: 2,
-        damage:4,
-        rend:4,
+        damage: 4,
+        rend: 4,
     },
     halberd: {
-        skill:3,
-        speed:2,
-        damage:4,
-        rend:3,
+        skill: 3,
+        speed: 2,
+        damage: 4,
+        rend: 3,
     }
 }
 
 let armour =
-    {
+{
     leather: {
-        skill:2,
-        speed:0,
-        protection:2,        
+        skill: 2,
+        speed: 0,
+        protection: 2,
     },
     chainmail: {
-        skill:2,
-        speed:-1,
-        protection:3,  
+        skill: 2,
+        speed: -1,
+        protection: 3,
     },
     plate: {
-        skill:1,
-        speed:-2,
-        protection:4,        
+        skill: 1,
+        speed: -2,
+        protection: 4,
     },
- }
+}
 
 let items = {
     healingPotion: {
@@ -96,6 +97,7 @@ let items = {
     },
     weakHealingPotion: {
         itemName: "2. Weak healing potion",
+        quantity: 1,
         stamina: 5,
     },
     fireBomb: {
@@ -108,15 +110,12 @@ let items = {
         quantity: 1,
         damage: 7,
     },
- };
-
+};
 
 let weaponsInventoryLength = Object.keys(weapons).length;
 let armourInventoryLength = Object.keys(armour).length;
 
 // console.log("Weapons Inventory Length = " + weaponsInventoryLength);
-
-//
 let heroWeapon = weapons.sword
 
 //random weapons and armour for the monster
@@ -139,8 +138,10 @@ let monsterArmourProtectionBonus = armour[randomArmour].protection;
 let hero = new Player("elf",/*skill*/12,/*stamina*/12,/*luck*/12,/*damage*/4,/*speed*/10,/*toughness*/6,/*armour*/0, ["sword", "shield", "leather armour"]);
 let lizardman = new Monster("Lizardman",/*skill*/12,/*stamina*/14,/*luck*/10,/*damage*/4,/*speed*/10,/*toughness*/7,/*armour*/0, []);
 let hellHornChampion = new Monster("Hellhorn Champion",/*skill*/14,/*stamina*/16,/*luck*/10,/*damage*/4,/*speed*/12,/*toughness*/9,/*armour*/0, []);
-let beastman = new Monster("beastman", 8, 16, 8, 4, 6, 10, 0,[]);
+let beastman = new Monster("beastman", 8, 16, 8, 4, 6, 10, 0, []);
 let ogre = new Monster("Ogre", 7, 16, 7, 5, 6, 12, 0, []);
+
+//hero initial values:  
 
 //array of all possible monsters
 let possibleMonsters = [lizardman, hellHornChampion, beastman, ogre];
@@ -151,7 +152,7 @@ let randomMonsterIndex = Math.floor(Math.random() * possibleMonsters.length);
 let randomMonster = possibleMonsters[randomMonsterIndex];
 let randomMonsterType = possibleMonsters[randomMonsterIndex].type;
 console.log("the monster is a " + randomMonsterType);
-console.log(`The ${randomMonster.type} carries a rusty ` + randomWeapon + " (skill +" + monsterWeaponsSkillBonus + ")" );
+console.log(`The ${randomMonster.type} carries a rusty ` + randomWeapon + " (skill +" + monsterWeaponsSkillBonus + ")");
 console.log(`The ${randomMonster.type} is wearing battered ` + randomArmour + " armour" + " (skill +" + monsterArmourSkillBonus + ", protection +" + monsterArmourProtectionBonus + ")");
 randomMonster.items = [randomWeapon, randomArmour];
 console.log(randomMonster);
@@ -166,32 +167,60 @@ function randomD12() {
 function getHeroAttackStrength() {
     randomD12();
     heroAttackStrength = hero.skill + randomD12Num + heroWeapon.skill;
-    console.log("Hero attack strength = " + heroAttackStrength );
+    console.log("Hero attack strength = " + heroAttackStrength);
 }
 
 function getMonsterAttackStrength() {
     randomD12();
-    monsterAttackStrength = randomMonster.skill + randomD12Num + monsterWeaponsSkillBonus + monsterArmourSkillBonus; 
+    monsterAttackStrength = randomMonster.skill + randomD12Num + monsterWeaponsSkillBonus + monsterArmourSkillBonus;
     console.log(`${randomMonsterType} attack strength = ` + monsterAttackStrength);
     // console.log(monsterWeaponsBonus);
 }
 
-function useItem () {
-    console.log('Use an Item? select the corresponding number of the item you want to use or 0 for no');
-     // Iterate over the properties of the items object
-for (let key in items) {
-    if (items.hasOwnProperty(key)) {
-        console.log(items[key].itemName);
+function useItem() {
+    const useItemPrompt = prompt('Use Item (Y) or (N)?');
+    let useItemPromptAnswer = `${useItemPrompt}`;
+    console.log(useItemPromptAnswer);
+
+    if (useItemPromptAnswer === "Y" || useItemPromptAnswer === "y") {
+        // console.log("fight again!");
+        useWhichItem();
+    } else if (useItemPromptAnswer === "N" || useItemPromptAnswer === "n") {
+        console.log("No item used");
+    } else {
+        console.log("that's not a Y/N answer, please choose again");
+        useItem();
     }
+    console.log("----------------------------------");
 }
-const useItem = prompt('select item number (or 0 to skip): ');
-}
+
+function useWhichItem() {
+    
+    console.log('Select the corresponding number of the item you want to use or 0 for no');
+    // Iterate over the properties of the items object
+    for (let key in items) {
+        if (items.hasOwnProperty(key)) {
+            console.log(items[key].itemName + " quantity = " + items[key].quantity);
+        }
+    }
+
+    const useItem = prompt('select item number (0 to skip): ');
+    itemResponse = `${useItem}`;
+    if (itemResponse === "0") { console.log("No item used"); }
+    else if (itemResponse === "1") { console.log("Strong healing potion has been drunk! Stamina restored to full!"); }
+    else if (itemResponse === "2") { console.log("Weak healing potion has been drunk! 8 Stamina restored!"); }
+    else if (itemResponse === "3") { 
+        console.log("Fire bomb has been thrown!"); 
+        console.log(`The ${randomMonsterType} suffers ${items.fireBomb.damage} damage`);
+        currentMonsterStamina -= 5;
+        console.log(currentMonsterStamina); }
+
+        else if (itemResponse === "4") { console.log("Poison wind globe has been thrown!"); }
+};
 
 function fight() {
-
-    battleRound+=1;
-    
-    console.log("----- Battle round " + battleRound + " -----");
+    battleRound += 1;
+    console.log("--------- Battle round " + battleRound + " ---------");
     useItem();
     if (currentMonsterStamina == 0 || currentMonsterStamina < 0) {
         console.log(`The ${randomMonsterType} lies dead at your feet`);
@@ -215,7 +244,6 @@ function fight() {
                 return
             }
 
-
             // asks if you want to fight again
             fightAgainYorN();
 
@@ -227,11 +255,13 @@ function fight() {
             hero.stamina -= 2; //change this value to whatever the damage characteristic is of the monster
             console.log(`${randomMonster.type} wounds the hero, eeeeek!`)
             console.log("Hero stamina = " + hero.stamina);
-            
+
             if (hero.stamina == 0 || hero.stamina < 0) {
                 console.log("You Died!");
                 return
             }
+
+            console.log("----------------------------------");
             // reduce hero health 
             fightAgainYorN();
         }
